d5cf57add9f7ec20557609c8b578cef9f2353517
MathWistLight <hikari.masumitsu@gmail.com>
2019-03-03 15:07:29 +0900

    fix setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%r^or)da$_pk=z-s=tmsb&)(fxc(@(tfpzthh-pec^70b38@0d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #ローカルホストチェックが可能に
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # ローカルホストチェックが可能に
